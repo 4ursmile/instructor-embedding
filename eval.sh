f7ed22f9e1b94264596ed14ae4b06d1bfdc9d686
Bao Ly <drawtheworld999@gmail.com>
2023-12-19 20:06:02 +0700

    add bash script for evaluation

---
+++ eval.sh
@@ -0,0 +1,4 @@
+#!/bin/bash#
+cd evaluation/MTEB
+python examples/evaluate_model.py --model_name "hkunlp/instructor-&=$1" --output_dir outputs --task_name $2 --result_file results
+cd ../..
---
+++ eval_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash#
+cd evaluation/MTEB
+pip install -e .
+cd ../..
