9db46cff40a117caedf22b00eb23c038dc5a2824
Bao Ly <drawtheworld999@gmail.com>
2023-12-21 00:03:42 +0700

    add notebook

--- eval_billboard.sh
+++ eval_billboard.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd evaluation/text_evaluation
-python main.py --cache cache --model_name "hkunlp/instructor-$1" --task $2 --add_prompt
+python main.py --cache cache --model_name "hkunlp/instructor-$1" --task $2 
 cd ../..
